ecec380c60a5892c7c692392791a6058d18ba5bf
jf.sanchezg <jf.sanchezg@ISIS2603S2-0030.sis.virtual.uniandes.edu.co>
2017-09-12 12:10:38 -0500

    Errores mesaPU y reservaPU corregidos

--- restaurante-back/src/main/java/co/edu/uniandes/baco/restaurante/persistence/ReservaPersistence.java
+++ restaurante-back/src/main/java/co/edu/uniandes/baco/restaurante/persistence/ReservaPersistence.java
@@ -22,7 +22,7 @@ import javax.persistence.TypedQuery;
 public class ReservaPersistence {
     private static final Logger LOGGER = Logger.getLogger(ReservaPersistence.class.getName());
 
-    @PersistenceContext(unitName = "reservaPU")
+    @PersistenceContext(unitName = "restaurantePU")
     protected EntityManager em;
 
     /**
