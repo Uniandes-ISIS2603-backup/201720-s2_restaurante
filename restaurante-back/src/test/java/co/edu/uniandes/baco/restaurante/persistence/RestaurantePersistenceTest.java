217da8d150751ce211494f22ee7eac5c292cb240
afBejarano <af.bejarano@uniandes.edu.co>
2017-09-12 20:49:55 -0500

    Con las clases mias 3

--- restaurante-back/src/test/java/co/edu/uniandes/baco/restaurante/persistence/RestaurantePersistenceTest.java
+++ restaurante-back/src/test/java/co/edu/uniandes/baco/restaurante/persistence/RestaurantePersistenceTest.java
@@ -12,20 +12,21 @@ import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.UserTransaction;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.After;
 import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
-import org.jboss.arquillian.junit.Arquillian;
 import org.junit.runner.RunWith;
-import org.jboss.arquillian.container.test.api.Deployment;
-import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.spec.JavaArchive;
-import org.junit.Assert;
 import uk.co.jemos.podam.api.PodamFactory;
 import uk.co.jemos.podam.api.PodamFactoryImpl;
+
+
 /**
  *
  * @author jd.correa
--- restaurante-back/src/test/java/co/edu/uniandes/baco/restaurante/persistence/TarjetaPuntosPersistenceTest.java
+++ restaurante-back/src/test/java/co/edu/uniandes/baco/restaurante/persistence/TarjetaPuntosPersistenceTest.java
@@ -12,6 +12,8 @@ import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.UserTransaction;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.After;
@@ -20,16 +22,28 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
 import uk.co.jemos.podam.api.PodamFactory;
 import uk.co.jemos.podam.api.PodamFactoryImpl;
+
+
 
 /**
  *
  * @author af.bejarano
  */
+@RunWith(Arquillian.class)
 public class TarjetaPuntosPersistenceTest {
-    
+    @Inject
+    private TarjetaPuntosPersistence persistence;
+    @PersistenceContext
+    private EntityManager em;
+    @Inject
+    UserTransaction utx;
+    private List<TarjetaPuntosEntity> data = new ArrayList<TarjetaPuntosEntity>();
+    public TarjetaPuntosPersistenceTest() {
+    }
+    @Deployment
     public static JavaArchive createDeployment() {
         return ShrinkWrap.create(JavaArchive.class)
                 .addPackage(TarjetaPuntosEntity.class.getPackage())
@@ -37,44 +51,74 @@ public class TarjetaPuntosPersistenceTest {
                 .addAsManifestResource("META-INF/persistence.xml", "persistence.xml")
                 .addAsManifestResource("META-INF/beans.xml", "beans.xml");
     }
-    
-    /**
-     * Inyección de la dependencia a la clase XYZPersistence cuyos métodos
-     * se van a probar.
-     */
-    @Inject
-    private TarjetaPuntosPersistence persistence;
-
-    /**
-     * Contexto de Persistencia que se va a utilizar para acceder a la Base de
-     * datos por fuera de los métodos que se están probando.
-     */
-    @PersistenceContext
-    private EntityManager em;
-
-    /**
-     * Variable para martcar las transacciones del em anterior cuando se
-     * crean/borran datos para las pruebas.
-     */
-    @Inject
-    UserTransaction utx;
-
-     /**
-     *
-     */
-    private List<TarjetaPuntosEntity> data = new ArrayList<TarjetaPuntosEntity>();
-    
-    public TarjetaPuntosPersistenceTest() {
+    private void clearData() {
+        em.createQuery("delete from TarjetaPuntosEntity").executeUpdate();
     }
     
-    @BeforeClass
-    public static void setUpClass() {
+
+
+    private void insertData() {
+        PodamFactory factory = new PodamFactoryImpl();
+        for (int i = 0; i < 3; i++) {
+            TarjetaPuntosEntity entity = factory.manufacturePojo(TarjetaPuntosEntity.class);
+
+            em.persist(entity);
+            data.add(entity);
+        }
     }
-    
-    @AfterClass
-    public static void tearDownClass() {
+    @Test
+    public void createTarjetaPuntosEntityTest() {
+    PodamFactory factory = new PodamFactoryImpl();
+    TarjetaPuntosEntity newEntity = factory.manufacturePojo(TarjetaPuntosEntity.class);
+    TarjetaPuntosEntity result = persistence.create(newEntity);
+
+    Assert.assertNotNull(result);
+    TarjetaPuntosEntity entity = em.find(TarjetaPuntosEntity.class, result.getId());
+    Assert.assertNotNull(entity);
+    Assert.assertEquals(newEntity.getName(), entity.getName());
+}
+    @Test
+public void getTarjetaPuntossTest() {
+    List<TarjetaPuntosEntity> list = persistence.findAll();
+    Assert.assertEquals(data.size(), list.size());
+    for (TarjetaPuntosEntity ent : list) {
+        boolean found = false;
+        for (TarjetaPuntosEntity entity : data) {
+            if (ent.getId().equals(entity.getId())) {
+                found = true;
+            }
+        }
+        Assert.assertTrue(found);
     }
-    
+}
+@Test
+public void getTarjetaPuntosTest() {
+    TarjetaPuntosEntity entity = data.get(0);
+    TarjetaPuntosEntity newEntity = persistence.find(entity.getId());
+    Assert.assertNotNull(newEntity);
+    Assert.assertEquals(entity.getName(), newEntity.getName());
+}
+@Test
+public void updateTarjetaPuntosTest() {
+    TarjetaPuntosEntity entity = data.get(0);
+    PodamFactory factory = new PodamFactoryImpl();
+    TarjetaPuntosEntity newEntity = factory.manufacturePojo(TarjetaPuntosEntity.class);
+
+    newEntity.setId(entity.getId());
+
+    persistence.update(newEntity);
+
+    TarjetaPuntosEntity resp = em.find(TarjetaPuntosEntity.class, entity.getId());
+
+    Assert.assertEquals(newEntity.getName(), resp.getName());
+}
+@Test
+public void deleteTarjetaPuntosTest() {
+    TarjetaPuntosEntity entity = data.get(0);
+    persistence.delete(entity.getId());
+    TarjetaPuntosEntity deleted = em.find(TarjetaPuntosEntity.class, entity.getId());
+    Assert.assertNull(deleted);
+}
     @Before
     public void setUp() {
         try {
@@ -92,83 +136,15 @@ public class TarjetaPuntosPersistenceTest {
             }
         }
     }
-    
-    private void clearData() {
-        em.createQuery("delete from TarjetaPuntosEntity").executeUpdate();
+    @BeforeClass
+    public static void setUpClass() {
     }
-
-
- private void insertData() {
-        PodamFactory factory = new PodamFactoryImpl();
-        for (int i = 0; i < 3; i++) {
-            TarjetaPuntosEntity entity = factory.manufacturePojo(TarjetaPuntosEntity.class);
-
-            em.persist(entity);
-            data.add(entity);
-        }
+    
+    @AfterClass
+    public static void tearDownClass() {
     }
     
     @After
     public void tearDown() {
     }
-
-    /**
-     * Test of create method, of class TarjetaPuntosPersistence.
-     */
-    @Test
-    public void createTarjetaPuntosEntityTest() {
-    PodamFactory factory = new PodamFactoryImpl();
-    TarjetaPuntosEntity newEntity = factory.manufacturePojo(TarjetaPuntosEntity.class);
-    TarjetaPuntosEntity result = persistence.create(newEntity);
-
-    Assert.assertNotNull(result);
-    TarjetaPuntosEntity entity = em.find(TarjetaPuntosEntity.class, result.getId());
-    Assert.assertNotNull(entity);
-    Assert.assertEquals(newEntity.getName(), entity.getName());
-}
-
-@Test
-public void getTarjetasPuntosTest() {
-    List<TarjetaPuntosEntity> list = persistence.findAll();
-    Assert.assertEquals(data.size(), list.size());
-    for (TarjetaPuntosEntity ent : list) {
-        boolean found = false;
-        for (TarjetaPuntosEntity entity : data) {
-            if (ent.getId().equals(entity.getId())) {
-                found = true;
-            }
-        }
-        Assert.assertTrue(found);
-    }
-}
-    @Test
-public void getTarjetaPuntosTest() {
-    TarjetaPuntosEntity entity = data.get(0);
-    TarjetaPuntosEntity newEntity = persistence.find(entity.getId());
-    Assert.assertNotNull(newEntity);
-    Assert.assertEquals(entity.getName(), newEntity.getName());
-}
-
-@Test
-public void updateTarjetaPuntosTest() {
-    TarjetaPuntosEntity entity = data.get(0);
-    PodamFactory factory = new PodamFactoryImpl();
-    TarjetaPuntosEntity newEntity = factory.manufacturePojo(TarjetaPuntosEntity.class);
-
-    newEntity.setId(entity.getId());
-
-    persistence.update(newEntity);
-
-    TarjetaPuntosEntity resp = em.find(TarjetaPuntosEntity.class, entity.getId());
-
-    Assert.assertEquals(newEntity.getName(), resp.getName());
-}
-
-@Test
-public void deleteXYZTest() {
-    TarjetaPuntosEntity entity = data.get(0);
-    persistence.delete(entity.getId());
-    TarjetaPuntosEntity deleted = em.find(TarjetaPuntosEntity.class, entity.getId());
-    Assert.assertNull(deleted);
-}
 }
