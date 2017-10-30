f2632f18c1052f53a156667217cd2df86b084f14
afBejarano <af.bejarano@uniandes.edu.co>
2017-10-30 11:51:49 -0500

    dommicilio modulo (falta agregar el pedido dentro del domicilio)

---
+++ restaurante-web/src/main/webapp/src/modules/domicilios/domicilios.mod.js
@@ -0,0 +1,19 @@
+(function (ng){
+   var mod=ng.module("domicilioModule",['ui.router']);
+   
+   mod.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+            var basePath = 'src/modules/domicilios/';
+            $urlRouterProvider.otherwise("/domiciliosList");
+            $stateProvider.state('domiciliosList', {
+                url: '/domicilios/list',
+                views: {
+                    'mainView': {
+                        templateUrl: basePath + 'domicilios.list.html',
+                        controller: 'domicilioCtrl',
+                        controllerAs: 'ctrl'
+                    }
+                }
+            });
+        }
+    ]);
+})(window.angular);
