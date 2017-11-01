9144846354f4c962576ecb564788640295bfb1c3
jf.sanchezg <jf.sanchezg@ISIS2603S2-0030.sis.virtual.uniandes.edu.co>
2017-11-01 15:33:57 -0500

    Modulo Reserva al fin funciona.

--- restaurante-web/src/main/webapp/src/modules/reservas/reservas.ctrl.js
+++ restaurante-web/src/main/webapp/src/modules/reservas/reservas.ctrl.js
@@ -1,17 +1,17 @@
 (function (ng) {
     var mod = ng.module("reservaModule");
     mod.constant("reservasContext", "api/reservas");
-    mod.controller('reservasCtrl', ['$scope', '$http', 'reservasContext',
+    mod.controller('reservasCtrl', ['$scope', '$http', 'reservasContext','$state',
         function ($scope, $http, reservasContext, $state) {
             $http.get('data/reservas.json').then(function (response) { 
                 $scope.reservasRecords = response.data;
             });
             
-//            if (($state.params.reservaId !== undefined) && ($state.params.reservaId !== null)) {
-//                $http.get(reservasContext + '/' + $state.params.reservaId).then(function (response) {
-//                    $scope.currentReserva = response.data;
-//                });
-//            }
+            if (($state.params.reservaId !== undefined) && ($state.params.reservaId !== null)) {
+                $http.get(reservasContext + '/' + $state.params.reservaId).then(function (response) {
+                    $scope.currentReserva = response.data;
+                });
+            }
         }
     ]);
 }
