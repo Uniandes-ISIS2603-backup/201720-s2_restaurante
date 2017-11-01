2ce63e49b75838f1da31a7631d89d702e39ad51d
Juan Diego Correa <jd.correa@uniandes.edu.co>
2017-10-31 20:16:57 -0500

    Arreglado bug de sonar cube de delete. Se SUPONE lo mio ya está

--- restaurante-web/src/main/webapp/src/modules/restaurantes/delete/restaurantes.delete.ctrl.js
+++ restaurante-web/src/main/webapp/src/modules/restaurantes/delete/restaurantes.delete.ctrl.js
@@ -8,7 +8,6 @@
     mod.constant("restaurantesContext", "api/restaurantes");
     mod.controller('restauranteDeleteCtrl', ['$scope', '$http', 'restaurantesContext', '$state',
         function ($scope, $http, restaurantesContext, $state) {
-            var idrestaurante = $state.params.restauranteId;
             $scope.deleteRestaurante = function () {
                 $http.delete(restaurantesContext + '/' + idRestaurante, {}).then(function (response) {
                     $state.go('restaurantesList', {restauranteId: response.data.id}, {reload: true});
