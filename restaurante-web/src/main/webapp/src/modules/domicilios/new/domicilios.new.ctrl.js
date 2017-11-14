59bd4de4ae7235129a1e19b048db85b07da5026f
afBejarano <af.bejarano@uniandes.edu.co>
2017-11-14 11:55:51 -0500

    Domicilio implementado con error

---
+++ restaurante-web/src/main/webapp/src/modules/domicilios/new/domicilios.new.ctrl.js
@@ -0,0 +1,22 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+(function (ng) {
+    var mod = ng.module("domicilioModule");
+    mod.constant("domiciliosContext", "api/domicilios");
+    mod.controller('domicilioNewCtrl', ['$scope', '$http', 'rdomiciliosContext', '$state', 'domiciliosContext', '$rootScope',
+        function ($scope, $http, domiciliosContext, $state, $rootScope) {
+            $rootScope.edit = false;
+            $scope.createdomicilio = function () {
+                $http.post(domiciliosContext, {
+                }).then(function (response) {
+                    //restaurante created successfully
+                    $state.go('domiciliosList', {domicilioId: response.data.id}, {reload: true});
+                });
+            };
+        }
+    ]);
+}
+)(angular);
