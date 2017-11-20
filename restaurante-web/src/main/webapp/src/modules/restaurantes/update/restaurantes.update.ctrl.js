aa440f86359bd7b84eb96aedc6b4b75be5fdf073
Daniel Felipe Peña <df.penap@uniandes.edu.co>
2017-11-20 12:29:30 -0500

    correccion  con Juan Diego restaurante funciona

--- restaurante-web/src/main/webapp/src/modules/restaurantes/update/restaurantes.update.ctrl.js
+++ restaurante-web/src/main/webapp/src/modules/restaurantes/update/restaurantes.update.ctrl.js
@@ -1,7 +1,7 @@
 (
         function (ng) {
             var mod = ng.module("restauranteModule");
-            mod.constant("restaurantesContext", "api/sucursales");
+            mod.constant("restaurantesContext", "http://localhost:8080/restaurante-web/api/sucursales");
             mod.controller('restauranteUpdateCtrl', ['$scope', '$http', 'restaurantesContext', '$state', '$rootScope', '$filter',
                 function ($scope, $http, restaurantesContext, $state, $rootScope, $filter) {
                     $rootScope.edit = true;
@@ -31,7 +31,10 @@
                         direccion: $scope.restauranteDireccion,
                         dosPisos: $scope.restauranteDosPisos,
                         calificacion: $scope.restauranteCalificacion
-                        });
+                        }).then(function (response) {
+                    //restaurante created successfully
+                    $state.go('restaurantesList', {restauranteId: response.data.id}, {reload: true});
+                });
                     };
                 }
             ]);
