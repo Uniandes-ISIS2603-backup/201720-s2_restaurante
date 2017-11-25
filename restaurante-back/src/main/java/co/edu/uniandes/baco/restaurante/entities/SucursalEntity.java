8387694a801fc34b0d617b765f8403263e8a4756
afBejarano <af.bejarano@uniandes.edu.co>
2017-11-24 20:03:05 -0500

    Solucionando deuda tecnica 3

--- restaurante-back/src/main/java/co/edu/uniandes/baco/restaurante/entities/SucursalEntity.java
+++ restaurante-back/src/main/java/co/edu/uniandes/baco/restaurante/entities/SucursalEntity.java
@@ -8,7 +8,6 @@ package co.edu.uniandes.baco.restaurante.entities;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import javax.persistence.Entity;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
