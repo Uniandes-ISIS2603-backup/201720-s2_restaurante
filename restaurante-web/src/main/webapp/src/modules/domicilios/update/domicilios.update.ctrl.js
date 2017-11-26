716915462142c5a35cd725e729c8542c60e436b2
afBejarano <af.bejarano@uniandes.edu.co>
2017-11-25 20:51:47 -0500

    Solucionando errores(1)

--- restaurante-web/src/main/webapp/src/modules/domicilios/update/domicilios.update.ctrl.js
+++ restaurante-web/src/main/webapp/src/modules/domicilios/update/domicilios.update.ctrl.js
@@ -3,7 +3,7 @@
             var mod = ng.module("domicilioModule");
             mod.constant("domiciliosContext", "http://localhost:8080/restaurante-web/api/domicilios");
             mod.controller('domicilioUpdateCtrl', ['$scope', '$http', 'domiciliosContext', '$state', '$rootScope', '$filter',
-                function ($scope, $http, domiciliosContext, $state, $rootScope,) {
+                function ($scope, $http, domiciliosContext, $state, $rootScope) {
                     $rootScope.edit = true;
                     var idDomicilio = $state.params.domicilioId;                  
                     $http.get(domiciliosContext + '/' + idDomicilio).then(function (response) {
